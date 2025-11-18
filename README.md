020b0f9da83ded1a5a0e154104f4438b2a1943a0
sabbir53rahman <sabbir53rahman@gmail.com>
2025-11-18 20:24:08 +0600

    Refactor code for clarity and consistency in TypeScript solutions

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # next-level-assignment-1
+
+What are some differences between interfaces and types in TypeScript?
+
+Typescript এর মধ্যে type alias এবং interface  প্রায় একই রকম কাজ করলেও এর মধ্যে কিছু পার্থক্য রয়েছে
+যেমন :
+১. Interface মূলত object গঠনে সাহায্য করে কিন্তু type alias primitive data থেকে শুরু করে প্রায় সবকিছুই ডিফাইন করা যায় 
+2. interface এ এক নামে বারবার data ডিফাইন করা যায় এবং তা একটা data type হিসেবে কাজ করে কিন্তু type alias এ এক নামে বার বার data define করা যায় না
+3. Interface এ primitive data define করা যায় না কিন্তু type alias এ primitive data define করা যায়
+
+
+Explain the difference between any, unknown, and never types in TypeScript.
+
+* any type বলতে বুঝানো হয় এখানে যেকোনো টাইপের value আসতে পারে এবং any type ব্যবহার করলে এটি একটি variable এর মত  হয়ে যায় 
+* আর অন্যদিকে unkhown মূলত ব্যবহার হয় যখন আমি data type সম্পর্কে অবগত না কিন্তু রান টাইমে যেয়ে বুঝতে পারবো এটা কি type এর data আসবে 
+* আর never type বলতে বোঝানো হয় যেটা কখনোই কোন কিছুই রিটার্ন করবে না বা কোন কিছুই দিবে না কোন value দিবে না
--- solution.ts
+++ solution.ts
@@ -1,127 +1,139 @@
 // Problem 1
 
-
-const formatValue = (value:string | number | boolean) : string | number | boolean => {
- if(typeof value === "string" ){
- return value.toUpperCase();   
- }else if(typeof value=== "number"){
- return value * 10;
-} else if (typeof value === "boolean") {
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
     return value ? false : true;
-}else{
+  } else {
     return `This ${value} is not a correct type`;
-}
-}
-
+  }
+};
 
 // Problem 2
 
- 
 const getLength = (value: string | any[]): number => {
   return value.length;
-}
+};
 
-
-// Problem 3 
+// Problem 3
 
 class Person {
-    name:string;
-    age:number;
+  name: string;
+  age: number;
 
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
-    constructor(name:string, age:number){
-        this.name= name;
-        this.age = age;
-    }
-
-    getDetails(): string {
-        return `'Name: ${this.name}, Age: ${this.age}';`;
-    }
-} 
-
-
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}';`;
+  }
+}
 
 // Problem 4
- 
- type Items ={
-    title:string;
-    rating:number;
- }
 
+type Items = {
+  title: string;
+  rating: number;
+};
 
-
-const filterByRating = (value:Items[ ]) : Items[ ] | string=>{
-    if(value.some(item => item.rating > 5)){
-        return "Your rating is greater then 5";
-    }else{
-        return value.filter((item) => item.rating >= 4)
-    }
-} 
-
-
-
+const filterByRating = (value: Items[]): Items[] | string => {
+  if (value.some((item) => item.rating > 5)) {
+    return "Your rating is greater then 5";
+  } else {
+    return value.filter((item) => item.rating >= 4);
+  }
+};
 
 // Problem 5
 
-type User ={
-    id: number;
-    name : string;
-    email: string;
-    isActive: boolean;
-}
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
-const filterActiveUsers = (value :User[]): User[] =>{
-   return value.filter((user)=> user.isActive=== true) ;
+const filterActiveUsers = (value: User[]): User[] => {
+  return value.filter((user) => user.isActive === true);
+};
 
-}
-
-
-// Problem 6 
-
+// Problem 6
 
 interface Book {
-    title: string;
-    author: string;
-    publishedYear: number;
-    isAvailable: boolean;
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
 }
 
-const printBookDetails =(value:Book): void =>{
-    console.log(`Title: ${value.title}, Author: ${value.author}, Published: ${value.publishedYear}, Available: ${value.isAvailable ? "Yes" : "No"}`)
-}
-
+const printBookDetails = (value: Book): void => {
+  console.log(
+    `Title: ${value.title}, Author: ${value.author}, Published: ${
+      value.publishedYear
+    }, Available: ${value.isAvailable ? "Yes" : "No"}`
+  );
+};
 
 // Problem 7
 
+const getUniqueValues = (arr1: number[], arr2: number[]): number[] => {
+  let uniqueArray: number[] = [];
+  let uniqueIndex = 0;
+
+  function isExist(arr: number[], value: number): boolean {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!isExist(uniqueArray, arr1[i])) {
+      uniqueArray[uniqueIndex] = arr1[i];
+      uniqueIndex++;
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (!isExist(uniqueArray, arr2[i])) {
+      uniqueArray[uniqueIndex] = arr2[i];
+      uniqueIndex++;
+    }
+  }
+
+  return uniqueArray;
+};
 
 
 
-
-
-
-
-
-// Problem 8 
+// Problem 8
 
 interface Product {
-    name: string;
-    price: number;
-    quantity: number;
-    discount?: number;
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
 }
 
 const calculateTotalPrice = (value: Product[]): number => {
-    const totalPrice = value.reduce((sum, product) => {
+  const totalPrice = value.reduce((sum, product) => {
+    const price = product.price * product.quantity;
 
-        const price = product.price * product.quantity;
+    const finalPrice = product.discount
+      ? price - (price * product.discount) / 100
+      : price;
 
-        const finalPrice = product.discount 
-            ? price - (price * product.discount) / 100
-            : price;
+    return sum + finalPrice;
+  }, 0);
 
-        return sum + finalPrice;
-    }, 0);
-
-    return totalPrice;
+  return totalPrice;
 };
-
